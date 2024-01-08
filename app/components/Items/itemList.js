75da2300ec06b484e23d6b97a940266910fcb0fd
amirqa17 <49957940+amirqa17@users.noreply.github.com>
2024-01-08 20:25:27 +0900

    Reconstructed and Redesigned ItemList

--- app/components/Items/itemList.js
+++ app/components/Items/itemList.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useEffect, useState} from "react";
 import ShoppingItem from "./Item";
 import { RiseLoader } from "react-spinners";
 import { shoppingItemsRef } from "../../../itemsfirebase";
@@ -21,6 +22,16 @@ function ShoppingList() {
   const [loading, setLoading] = useState(true);
   const [randomizedItems, setRandomizedItems] = useState([]);
 
+  const sortElements = [
+    { title: "All items", category: null },
+    { title: "Mouse", category: "mouse" },
+    { title: "Keyboard", category: "keyboard" },
+    { title: "Gamepad", category: "gamepad" },
+    { title: "Headset", category: "headset" },
+    { title: "Mousepad", category: "mousepad" },
+    { title: "Monitor", category: "monitor" },
+  ];
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -32,25 +43,19 @@ function ShoppingList() {
           item.id = doc.id;
           items.push(item);
         });
-
-        // Shuffle items and set the state
         setRandomizedItems(shuffleArray(items));
-        setLoading(false); // Mark data loading as complete
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
-        setLoading(false); // Mark data loading as complete even on error
+        setLoading(false);
       }
     };
-
-    fetchData(); // Call the fetchData function
-  }, []); // The empty dependency array ensures this effect runs once on mount
-
+    fetchData();
+  }, []);
   const handleSort = (type) => {
     setSortType(type);
   };
-
   const selectedClass = "bg-gray-800 text-white";
-
   const filteredItems = randomizedItems.filter((item) => {
     return !sortType || item.type === sortType;
   });
@@ -68,105 +73,37 @@ function ShoppingList() {
 
   return (
     <>
-      <div className="flex justify-center items-center py-4 mx-auto ">
-        <p className="text-gray-600 text-sm md:text-lg mx-auto">
-          {" "}
+       <div className="flex flex-wrap lg:w-full mx-auto justify-center bg-gray-100">
+        <div className="sub-navbar px-8 bg-gray-600  lg:w-full">
+        
+        <section className="sub-navbar xl:bg-gray-600 pt-4 pb-4 lg:w-full space-x-2 md:space-x-14 flex flex-wrap justify-center">
+  {sortElements.map((element) => (
+    <button
+      key={element.title}
+      onClick={() => handleSort(element.category)}
+      type="button"
+      className={`mb-2 md:mb-0 inline-block font-medium rounded-full py-2 px-4 transition-colors duration-200 ease-in-out ${
+        sortType === element.category
+          ? "bg-pink-500 text-white shadow-md hover:bg-pink-600"
+          : "bg-white text-gray-800 shadow-sm hover:bg-pink-100 hover:text-pink-500"
+      }`}
+    >
+      {element.title}
+    </button>
+  ))}
+</section>
+          </div>
+          {filteredItems.map((item) => (
+            <ShoppingItem key={item.id} item={item} />
+          ))}
+        </div>
+  
+
+      <section className="flex justify-center items-center py-4 mx-auto ">
+        <p className="text-gray-600 text-sm md:text-lg mx-auto ">
           <span className="font-bold">{filteredItems.length}</span> items found
         </p>
-      </div>
-
-      <div className="flex flex-wrap lg:w-full mx-auto  justify-center bg-gray-200">
-        <div className="sub-navbar px-8 bg-gray-100 pt-4 pb-4 lg:w-full">
-          <div className="flex flex-col items-center md:flex-row md:justify-center">
-            <button
-              onClick={() => handleSort(null)}
-              type="button"
-              className={`mb-2 md:mb-0 inline-block font-medium rounded-full py-2 px-4 mx-2 transition-colors duration-200 ease-in-out ${
-                sortType === null
-                  ? "bg-pink-500 text-white shadow-md hover:bg-pink-600"
-                  : "bg-white text-gray-800 shadow-sm hover:bg-pink-100 hover:text-pink-500"
-              }`}
-            >
-              All items
-            </button>
-
-            <button
-              onClick={() => handleSort("mouse")}
-              type="button"
-              className={`mb-2 md:mb-0 inline-block font-medium rounded-full py-2 px-4 mx-2 transition-colors duration-200 ease-in-out ${
-                sortType === "mouse"
-                  ? "bg-pink-500 text-white shadow-md hover:bg-pink-600"
-                  : "bg-white text-gray-800 shadow-sm hover:bg-pink-100 hover:text-pink-500"
-              }`}
-            >
-              Mouse
-            </button>
-
-            <button
-              onClick={() => handleSort("keyboard")}
-              type="button"
-              className={`mb-2 md:mb-0 inline-block font-medium rounded-full py-2 px-4 mx-2 transition-colors duration-200 ease-in-out ${
-                sortType === "keyboard"
-                  ? "bg-pink-500 text-white shadow-md hover:bg-pink-600"
-                  : "bg-white text-gray-800 shadow-sm hover:bg-pink-100 hover:text-pink-500"
-              }`}
-            >
-              Keyboard
-            </button>
-
-            <button
-              onClick={() => handleSort("gamepad")}
-              type="button"
-              className={`mb-2 md:mb-0 inline-block font-medium rounded-full py-2 px-4 mx-2 transition-colors duration-200 ease-in-out ${
-                sortType === "gamepad"
-                  ? "bg-pink-500 text-white shadow-md hover:bg-pink-600"
-                  : "bg-white text-gray-800 shadow-sm hover:bg-pink-100 hover:text-pink-500"
-              }`}
-            >
-              Gamepad
-            </button>
-
-            <button
-              onClick={() => handleSort("headset")}
-              type="button"
-              className={`mb-2 md:mb-0 inline-block font-medium rounded-full py-2 px-4 mx-2 transition-colors duration-200 ease-in-out ${
-                sortType === "headset"
-                  ? "bg-pink-500 text-white shadow-md hover:bg-pink-600"
-                  : "bg-white text-gray-800 shadow-sm hover:bg-pink-100 hover:text-pink-500"
-              }`}
-            >
-              Headset
-            </button>
-
-            <button
-              onClick={() => handleSort("mousepad")}
-              type="button"
-              className={`mb-2 md:mb-0 inline-block font-medium rounded-full py-2 px-4 mx-2 transition-colors duration-200 ease-in-out ${
-                sortType === "mousepad"
-                  ? "bg-pink-500 text-white shadow-md hover:bg-pink-600"
-                  : "bg-white text-gray-800 shadow-sm hover:bg-pink-100 hover:text-pink-500"
-              }`}
-            >
-              Mousepad
-            </button>
-
-            <button
-              onClick={() => handleSort("monitor")}
-              type="button"
-              className={`mb-2 md:mb-0 inline-block font-medium rounded-full py-2 px-4 mx-2 transition-colors duration-200 ease-in-out ${
-                sortType === "monitor"
-                  ? "bg-pink-500 text-white shadow-md hover:bg-pink-600"
-                  : "bg-white text-gray-800 shadow-sm hover:bg-pink-100 hover:text-pink-500"
-              }`}
-            >
-              Monitor
-            </button>
-          </div>
-        </div>
-        {filteredItems.map((item) => (
-          <ShoppingItem key={item.id} item={item} />
-        ))}
-      </div>
+      </section>
     </>
   );
 }
